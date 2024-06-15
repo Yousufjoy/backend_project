899da6288cc5068508f22d30e5934ab4832a63f7
yousufjoy <yousufj93006@gmail.com>
2024-06-16 01:19:12 +0600

    Introduction To Validation

--- src/app/moduels/student.model.ts
+++ src/app/moduels/student.model.ts
@@ -1,19 +1,18 @@
 import { Schema, model, connect } from 'mongoose'
 import { LocalGurdian, Student, UserName, gurdian } from './student.interface'
 
-
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'First name is required'], // custon required!
   },
   middleName: {
     type: String,
-    required: true,
+    required: [true, 'Middle name is requred'],
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, 'last name is requred'],
   },
 })
 
@@ -66,25 +65,46 @@ const localGurdianSchema = new Schema<LocalGurdian>({
 // 2. Create a Schema corresponding to the document interface.
 const studentSchema = new Schema<Student>({
   //<Student> generic = dynamically type define kore arguemnt hisebe niye
-  id: { type: String },
-  name: userNameSchema,
-  gender: ['male', 'female'], // enum : mongoose e ekta type ase jeita k bole enum koyekta value theke jekono ekta ney
+  id: { type: String, required: true, unique: true }, // unique: true :--duplicate value dibe na
+  name: {
+    type: userNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not valid', // {VALUE} = user theke dynamic value pawa
+    },
+    required: true,
+  }, // enum : mongoose e ekta type ase jeita k bole enum koyekta value theke jekono ekta ney
   dateOfBirth: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   contactNo: { type: String, required: true },
   emergencyContactNo: { type: String, required: true },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
   presentAddress: { type: String, required: true },
   parmanentAddress: { type: String, required: true },
-  gurdian: gurdianSchema,
-  localGurdian: localGurdianSchema,
+  gurdian: {
+    type: gurdianSchema,
+    required: true,
+  },
+  localGurdian: {
+    type: localGurdianSchema,
+    required: true,
+  },
   profileImage: {
     type: String,
   },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 })
 
-
 // 3. Create a Model.
-export const StudentModel = model<Student>('User', studentSchema); // Student= type, schema = studentSchema, name = User same as the const value
-
+export const StudentModel = model<Student>('User', studentSchema) // Student= type, schema = studentSchema, name = User same as the const value
