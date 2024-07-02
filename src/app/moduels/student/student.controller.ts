5657c981ada2111f4ba475114b223f773ccd78c8
yousufjoy <yousufj93006@gmail.com>
2024-07-02 15:42:02 +0600

    Avoid Repetition Of Try-Catch , Use CatchAsync

--- src/app/moduels/student/student.controller.ts
+++ src/app/moduels/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { StudentServices } from './student.service'
 import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
@@ -7,71 +7,47 @@ import httpStatus from 'http-status'
 
 //Handle korbe application logic, sudhu request/response handle korbe kivabe ashtese data mongodb/prisma etc eita dekhar bishoy na oita korbe service
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getAllStudentsFromDbB()
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Students Retrieved Successfully',
-      data: result,
-    })
-  } catch (err) {
-    {
-      next(err)
-    }
+const catchAsync = (fn: RequestHandler) => {
+  // We know higher order function parameter hisebe ekta function receive kore
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err))
   }
 }
 
-const deleteStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params
-    const result = await StudentServices.deleteStudentFromDB(studentId)
+const getAllStudents = catchAsync(async (req, res, next) => {
+  const result = await StudentServices.getAllStudentsFromDbB()
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students Retrieved Successfully',
+    data: result,
+  })
+})
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student Deleted Successfully',
-      data: result,
-    })
-  } catch (err) {
-    {
-      next(err)
-    }
-  }
-}
+const deleteStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params
+  const result = await StudentServices.deleteStudentFromDB(studentId)
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student Deleted Successfully',
+    data: result,
+  })
+})
 
-    const result = await StudentServices.getSingleStudent(studentId)
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Single Student data retrieved Successfully',
-      data: result,
-    })
-  } catch (err) {
-    {
-      next(err)
-    }
-  }
-}
+  const result = await StudentServices.getSingleStudent(studentId)
 
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Single Student data retrieved Successfully',
+    data: result,
+  })
+})
 export const StudentControllers = {
   getAllStudents,
   getSingleStudent,
--- src/app/moduels/user/user.controller.ts
+++ src/app/moduels/user/user.controller.ts
@@ -1,13 +1,12 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { UserService } from './user.services'
 import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+// RequestHandler :  req: Request,
+//res: Response,
+//next: NextFunction, Eita kore dicche
+const createStudent: RequestHandler = async (req, res, next) => {
   try {
     const { password, student: StudentData } = req.body
     const result = await UserService.createStudentIntoDB(password, StudentData)
